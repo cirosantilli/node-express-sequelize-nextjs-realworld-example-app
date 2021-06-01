ee1bf19d7cd2d83f765cf817f7a4029113f87c7b
Ciro Santilli <ciro.santilli@gmail.com>
2021-06-01 16:52:47 +0100

    database_url...

--- config/index.js
+++ config/index.js
@@ -3,6 +3,7 @@ const fs = require('fs')
 
 module.exports = {
   apiPath: '/api',
+  databaseUrl: process.env.DATABASE_URL || '',
   isProduction: process.env.NODE_ENV === 'production',
   secret: process.env.NODE_ENV === 'production' ? process.env.SECRET : 'secret',
   port: process.env.PORT || 3000,
