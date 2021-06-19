04abb2ba4971cf792091beba6b46b3863f2be5d9
Ciro Santilli <ciro.santilli@gmail.com>
2021-06-19 18:56:44 +0100

    demo data in production

--- bin/generate-demo-data.js
+++ bin/generate-demo-data.js
@@ -4,7 +4,6 @@ const assert = require('assert')
 const path = require('path')
 
 const config = require('../config')
-assert(!config.isProduction)
 
 function myParseInt(value, dummyPrevious) {
   const parsedValue = parseInt(value);
@@ -18,10 +17,14 @@ const commander = require('commander');
 commander.option('-a, --n-articles-per-user <n>', 'n articles per user', myParseInt, 10);
 commander.option('-f, --n-follows-per-user <n>', 'n follows per user', myParseInt, 2);
 commander.option('-t, --n-tags <n>', 'n favorites per user', myParseInt, 5);
+commander.option('--force-production', 'allow running in production, DELETES ALL DATA', false);
 commander.option('-u, --n-users <n>', 'n users', myParseInt, 10);
 commander.option('-v, --n-favorites-per-user <n>', 'n favorites per user', myParseInt, 5);
 commander.parse(process.argv);
 
+if (!commander.forceProduction) {
+  assert(!config.isProduction)
+}
 (async () => {
 const test_lib = require('../test_lib')
 const sequelize = await test_lib.generateDemoData({
