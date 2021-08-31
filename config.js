00610ce8490c5cbf610ed863763188a5890f7cb6
Ciro Santilli <ciro.santilli@gmail.com>
2021-08-31 22:33:05 +0100

    blacklist tags

--- config.js
+++ config.js
@@ -4,8 +4,12 @@ const fs = require('fs')
 module.exports = {
   apiPath: '/api',
   databaseUrl: process.env.DATABASE_URL || '',
+  isDemo: process.env.CIROSANTILLI_DEMO === 'true',
   isProduction: process.env.NODE_ENV === 'production',
   secret: process.env.NODE_ENV === 'production' ? process.env.SECRET : 'secret',
   port: process.env.PORT || 3000,
-  verbose: process.env.VERBOSE
+  verbose: process.env.VERBOSE,
+  blacklistTags: new Set([
+    'cypress',
+  ])
 }
