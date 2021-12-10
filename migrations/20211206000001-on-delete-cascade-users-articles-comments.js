9dfd3df0516071710e0e15b83f158635fb152fa6
Ciro Santilli <ciro.santilli@gmail.com>
2021-12-10 09:55:06 +0000

    Use transaction in migration

--- migrations/20211206000001-on-delete-cascade-users-articles-comments.js
+++ migrations/20211206000001-on-delete-cascade-users-articles-comments.js
@@ -1,13 +1,13 @@
 module.exports = {
   // https://github.com/sequelize/sequelize/issues/12789
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.changeColumn('Article', 'authorId', { onDelete: 'CASCADE' })
-    await queryInterface.changeColumn('Comment', 'articleId', { onDelete: 'CASCADE' })
-    await queryInterface.changeColumn('Comment', 'authorId', { onDelete: 'CASCADE' })
-  },
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.changeColumn('Article', 'authorId', { onDelete: 'NO ACTION' })
-    await queryInterface.changeColumn('Comment', 'articleId', { onDelete: 'NO ACTION' })
-    await queryInterface.changeColumn('Comment', 'authorId', { onDelete: 'NO ACTION' })
-  }
+  up: async (queryInterface, Sequelize) => queryInterface.sequelize.transaction(async transaction => {
+    await queryInterface.changeColumn('Article', 'authorId', { onDelete: 'CASCADE', transaction })
+    await queryInterface.changeColumn('Comment', 'articleId', { onDelete: 'CASCADE', transaction })
+    await queryInterface.changeColumn('Comment', 'authorId', { onDelete: 'CASCADE', transaction })
+  }),
+  down: async (queryInterface, Sequelize) => queryInterface.sequelize.transaction(async t => {
+    await queryInterface.changeColumn('Article', 'authorId', { onDelete: 'NO ACTION', transaction })
+    await queryInterface.changeColumn('Comment', 'articleId', { onDelete: 'NO ACTION', transaction })
+    await queryInterface.changeColumn('Comment', 'authorId', { onDelete: 'NO ACTION', transaction })
+  })
 };
