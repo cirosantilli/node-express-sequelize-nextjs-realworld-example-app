7de487247108a6b7f6db41db59da4435f55e914f
Ciro Santilli <ciro.santilli@gmail.com>
2021-12-10 18:20:38 +0000

    start a factored routes store

---
+++ routes.ts
@@ -0,0 +1,12 @@
+export default {
+  home: () => `/`,
+  articleEdit: slug => `/editor/${slug}`,
+  articleNew: () => `/editor`,
+  articleView: slug => `/article/${slug}`,
+  userEdit: () => `/settings`,
+  userLogin: () => `/user/login`,
+  userNew: () => `/user/register`,
+  userView: uid => `/profile/${uid}`,
+  userViewLikes: uid => `/profile/${uid}/favorites`,
+  userViewLatest: uid => `/user/latest/${uid}`,
+}
