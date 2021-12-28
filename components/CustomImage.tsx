b278cce8618e4606d60e8b2d2aaf5ee2096ac85a
Ciro Santilli <ciro.santilli@gmail.com>
2021-12-28 20:57:03 +0000

    Don't show alt text of broken images on index
    It overflows everywhere and annoys me.
    
    Proper resolution would be to show smiley instead:
    https://github.com/cirosantilli/node-express-sequelize-nextjs-realworld-example-app/issues/14

--- components/CustomImage.tsx
+++ components/CustomImage.tsx
@@ -1,6 +1,7 @@
 import { DEFAULT_PROFILE_IMAGE } from "lib/utils/constant";
 
 // https://stackoverflow.com/questions/34097560/react-js-replace-img-src-onerror
+// https://stackoverflow.com/questions/66949606/what-is-the-best-way-to-have-a-fallback-image-in-nextjs
 const handleBrokenImage = e => {
   e.target.src = DEFAULT_PROFILE_IMAGE;
   e.target.onerror = null;
@@ -14,14 +15,16 @@ interface CustomImageProps {
 
 const CustomImage = ({ src, alt, className }: CustomImageProps) => {
   const props: any = {}
+  const classes = ['hide-text']
   if (className) {
-    props.className = className
+    classes.push(className)
   }
   return (
     <img
       src={src}
       alt={alt}
       onError={handleBrokenImage}
+      className={classes.join(' ')}
       {...props}
     />
   )
