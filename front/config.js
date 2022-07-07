f99008dd94e65c0c8a781711cd3b3525db22b443
Yaroslav Kostenko <39385053+iBrianWarner@users.noreply.github.com>
2022-07-07 17:16:56 +0300

    Update config.js

--- front/config.js
+++ front/config.js
@@ -68,10 +68,10 @@ module.exports = {
       // https://stackoverflow.com/questions/27687546/cant-connect-to-heroku-postgresql-database-from-local-node-app-with-sequelize
       // https://devcenter.heroku.com/articles/heroku-postgresql#connecting-in-node-js
       // https://stackoverflow.com/questions/58965011/sequelizeconnectionerror-self-signed-certificate
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
+//       ssl: {
+//         require: true,
+//         rejectUnauthorized: false,
+//       },
     },
     logging: true,
   },
